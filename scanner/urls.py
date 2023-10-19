1f57ba351ad2a852af1808846c568df055f060b3
Amine Benziane <29580135+aminebz949904@users.noreply.github.com>
2023-10-19 11:23:16 +0100

    Update urls.py to use re_path

--- scanner/urls.py
+++ scanner/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 
 from .views import (home_page)
 
@@ -6,6 +6,6 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(pattern_name='home_page')),
+    re_path(r'^$', RedirectView.as_view(pattern_name='home_page')),
     path('home/', home_page.as_view(), name='home_page'),
 ]
